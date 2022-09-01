57fa15bd1cef7284b4f4c5ef83761c9691200fca
ramadhani059 <pratamaramadhani059@gmail.com>
2022-09-01 14:51:41 +0700

    React-Native QRCode Scanner

--- src/AbsensiScreen/AbsensiScreen.js
+++ src/AbsensiScreen/AbsensiScreen.js
@@ -1,27 +1,54 @@
 import React, { Component } from "react";
 import { View, Text, Button, StyleSheet, TouchableOpacity, Linking } from'react-native';
 
-import { RNCamera } from "react-native-camera";
+import QRCodeScanner from "react-native-qrcode-scanner";
+import { RNCamera } from 'react-native-camera';
 
 const AbsensiScreen = ({navigation}) => {
+    const onSuccess = (e) => {
+        Linking.openURL(e.data).catch(err =>
+            console.error('An error eccured', err)
+        );
+    };
+
     return(
-        <View style={styles.container}>
-            <Text> Absensi Screen </Text>
-            <Button 
-                title="Click Here"
-                onPress={() => alert('Button Clicked!')}
-            />
-        </View>
+        <QRCodeScanner 
+            onRead={onSuccess}
+            // flashMode={RNCamera.Constants.FlashMode.torch}
+            topContent={
+            <Text style={styles.centerText}>
+                Go to{' '}
+                <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+                your computer and scan the QR code.
+            </Text>
+            }
+            bottomContent={
+            <TouchableOpacity style={styles.buttonTouchable}>
+                <Text style={styles.buttonText}>OK. Got it!</Text>
+            </TouchableOpacity>
+            }
+        />
     );
 }
 
 export default AbsensiScreen;
 
 const styles = StyleSheet.create({
-    container:{
+    centerText: {
       flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#8fcbbc',
+      fontSize: 18,
+      padding: 32,
+      color: '#777'
     },
-});
+    textBold: {
+      fontWeight: '500',
+      color: '#000'
+    },
+    buttonText: {
+      fontSize: 21,
+      color: 'rgb(0,122,255)'
+    },
+    buttonTouchable: {
+      padding: 16
+    }
+  });
